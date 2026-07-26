7465c755efc9ba14e0b8787934addaa000dbce8b
Nikos Bako <nbakojohn13@gmail.com>
2026-07-26 16:33:56 +0200

    path bug for icons

--- src/components/playground-map.tsx
+++ src/components/playground-map.tsx
@@ -22,7 +22,7 @@ interface ClusterLike {
 }
 
 const playgroundIcon = new L.Icon({
-  iconUrl: "public/playground.png",
+  iconUrl: "playground.png",
   iconSize: [32, 32],
   iconAnchor: [16, 32],
   popupAnchor: [0, -32],
